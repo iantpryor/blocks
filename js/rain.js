15006efd26ce835bf25c62381a22e539b6d48a8a
iantpryor <iantpryor@gmail.com>
2015-06-19 09:47:38 -0400

    Update rain.js

--- js/rain.js
+++ js/rain.js
@@ -1,1 +1,39 @@
+function Rain() {
+  this.x = Math.floor(Math.random() * (512 - 0 + 1)) + 0;;
+  this.y = (480 - 30);
+  this.velocity = 5;
+  this.veleftright = 0;
+  this.tail = this.x + 30;
+};
 
+Rain.prototype.draw = function(context) {
+  //context.beginPath();
+  context.fillStyle = "#F00";
+  context.fillRect(this.x, this.y, 1, 1);
+  //context.strokeRect(this.x, this.y, 30, 30);
+  //context.stroke();
+  //context.closePath();
+};
+
+Rain.prototype.update = function() {
+  if (this.y > 288) {
+    this.y = - 5;
+    this.x = Math.floor(Math.random() * (512 - 0 + 1)) + 0;
+  }
+  if(player.input.left == true){
+    this.veleftright = 5;
+    if(player.input.jump == true){
+      this.veleftright += 5;
+    }
+  }else if(player.input.right == true){
+    this.veleftright = -5;
+    if(player.input.jump == true){
+      this.veleftright -= 5;
+    }
+  }else{
+    this.veleftright = 0;
+  }
+  
+  this.y += this.velocity;
+  this.x += this.veleftright;
+};
