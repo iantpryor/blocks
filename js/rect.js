47c4332be76597bbc15426a86ff0d89c933bde39
iantpryor <iantpryor@gmail.com>
2015-06-26 14:25:41 -0400

    Update rect.js

--- js/rect.js
+++ js/rect.js
@@ -4,6 +4,8 @@ function Rect(initX) {
   this.velocity = 0;
   this.veleftright = 0;
   this.tail = this.x + 32;
+  this.level = 0; //rand
+  this.prevLevel = this.level;
 };
 
 Rect.prototype.draw = function(context) {
@@ -15,12 +17,7 @@ Rect.prototype.draw = function(context) {
 };
 
 Rect.prototype.update = function() {
-  /*if (this.x < -32) {
-    this.x = Game.entites[16].tail;
-    
-  } else if (this.x > 512) {
-    this.x = -32;
-  }*/
+  
   
   if(player.input.left == true){
     this.veleftright = 5;
